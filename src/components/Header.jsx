c71a39a48a9b09f355032f4c7357a46f3f5d5971
Arthur <arthurteixeira0805@gmail.com>
2022-01-30 13:58:37 -0300

    💄 style: installed tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline text-red-500">Hello World</div>
+    </div>);
+};
+
+export default Header;
