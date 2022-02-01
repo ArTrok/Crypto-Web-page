f12fb06d04925f6d79e1c29ebee432f61f221b66
Arthur <arthurteixeira0805@gmail.com>
2022-02-01 10:44:48 -0300

    ⚡️ fix: still not working

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route exact path="/" render={App} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root'),
 );
