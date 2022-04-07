718b2096bda930cdeeb00719837c685a700a9a5e
Arthur <arthurteixeira0805@gmail.com>
2022-04-07 17:44:28 -0300

    🍱 add: converter comp and api address comment

---
+++ src/components/ConverterComp.jsx
@@ -0,0 +1,10 @@
+// https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
+import React from 'react';
+
+function ConverterComp() {
+  return (
+    <div>ConverterComp</div>
+  );
+}
+
+export default ConverterComp;
