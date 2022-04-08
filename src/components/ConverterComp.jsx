54774b813fe48fe6752066aa5a516684a56690c4
Arthur <arthurteixeira0805@gmail.com>
2022-04-08 17:55:55 -0300

    🚧 added: useState and useEffect

--- src/components/ConverterComp.jsx
+++ src/components/ConverterComp.jsx
@@ -1,7 +1,9 @@
 // https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 
 function ConverterComp() {
+  const [currencyPrice, setCurrencyPrice] = useState('');
   return (
     <div>ConverterComp</div>
   );
