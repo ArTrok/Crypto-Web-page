cb7e040345df153283414a1f180f6006426f54e6
Arthur <arthurteixeira0805@gmail.com>
2022-04-16 10:53:57 -0300

    🚧 wip: fetching

--- src/components/ConverterComp.jsx
+++ src/components/ConverterComp.jsx
@@ -1,11 +1,11 @@
-// https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
-
 import React, { useState, useEffect } from 'react';
+// https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
+const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 function ConverterComp() {
   const [currencyPrice, setCurrencyPrice] = useState('');
-  useEffect(() => {
-    first;
+  useEffect(async () => {
+    const currencyData = await fetch(URL);
   }, []);
 
   return (
