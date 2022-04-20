fef511de192976a303175f63cd3166b771c7c1a2
Arthur <arthurteixeira0805@gmail.com>
2022-04-20 16:05:46 -0300

    🚧 wip: nothing relevant

--- src/components/ConverterComp.jsx
+++ src/components/ConverterComp.jsx
@@ -1,3 +1,4 @@
+// not working yet
 import React, { useState, useEffect } from 'react';
 // https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
 const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
