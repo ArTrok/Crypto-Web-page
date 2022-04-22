79f7cbca63a9bdda969600fefcd21706b48a43f3
Arthur <arthurteixeira0805@gmail.com>
2022-04-22 18:48:23 -0300

    🔥 rm: comment removed

--- src/components/ConverterComp.jsx
+++ src/components/ConverterComp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// https://api.coindesk.com/v1/bpi/currentprice.json ---- api to be fetched
+
 const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 function ConverterComp() {
